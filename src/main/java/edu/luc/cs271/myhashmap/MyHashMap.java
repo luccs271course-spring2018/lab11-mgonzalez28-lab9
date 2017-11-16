ed915469df5ea55a166175d564d08d5d97ca72e2
Miguel Gonzalez <mgonzalez28@luc.edu>
2017-11-16 00:11:20 +0000

    Update to MyHashMap

--- src/main/java/edu/luc/cs271/myhashmap/MyHashMap.java
+++ src/main/java/edu/luc/cs271/myhashmap/MyHashMap.java
@@ -30,8 +30,8 @@ public class MyHashMap<K, V> implements Map<K, V> {
 
   @Override
   public int size() {
-    // TODO add the sizes of all the chains
-    int result = 0;
+    // DONE add the sizes of all the chains
+    int result = table.size()%DEFAULT_TABLE_SIZE;
 
 
     return result;
@@ -44,26 +44,45 @@ public class MyHashMap<K, V> implements Map<K, V> {
 
   @Override
   public boolean containsKey(final Object key) {
-    // TODO follow basic approach of remove below (though this will be much simpler)
+    // DONE follow basic approach of remove below (though this will be much simpler)
     final int index = calculateIndex(key);
-
-
+    final Iterator<Entry<K, V>> iter = table.get(index).iterator();
+    while (iter.hasNext()) {
+      final Entry<K, V> entry = iter.next();
+      if (entry.getKey().equals(key)) {
+        return true;
+      }
+    }
     return false;
   }
 
   @Override
   public boolean containsValue(final Object value) {
-    // TODO follow basic approach of remove below (though this will be much simpler)
-
-
+    // DONE follow basic approach of remove below (though this will be much simpler)
+    for (int i = 0; i < table.size(); i++) {
+      final Iterator<Entry<K, V>> iter = table.get(index).iterator();
+      while (iter.hasNext()) {
+        final Entry<K, V> entry = iter.next();
+        if (entry.getValue().equals(value)) {
+        return true;
+        }
+      }
+    }
     return false;
   }
 
   @Override
   public V get(final Object key) {
-    // TODO follow basic approach of remove below (though this will be simpler)
+    // DONE follow basic approach of remove below (though this will be simpler)
     final int index = calculateIndex(key);
-
+    final Iterator<Entry<K, V>> iter = table.get(index).iterator();
+    while (iter.hasNext()) {
+      final Entry<K, V> entry = iter.next();
+      if (entry.getKey().equals(key)) {
+        final v val = entry.getValue();
+        return val;
+      }
+    }
 
     return null;
   }
